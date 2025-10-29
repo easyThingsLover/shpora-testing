f02dc6390b6caa0dd1a091a9600526a2bfac485e
easyThingsLover <borderlandzbon@gmail.com>
2025-10-29 12:22:22 +0500

    Дропнул комменты, тесты валидатора поменял на тест кейсы, добавил несколько новых тестов

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -9,53 +9,45 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    [TestCase(-1, 2, true)]
+    [TestCase(0, 2, true)]
+    [TestCase(1, -1, true)]
+    [TestCase(1, 3, true)]
+    [TestCase(1, 1, true)]
+    public void Constructor_ThrowArgumentException_WhenParametersInvalid(int precision, int scale, bool onlyPositive)
     {
-        
-        /*Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));*/
-        using (new AssertionScope())
-        {
-            var precisionBelowZeroValidator = () => new NumberValidator(-1, 2, true);
-            var scaleBelowZeroValidator = () => new NumberValidator(1, -1, true);
-            var scaleGreaterThanPrecisionValidator = () => new NumberValidator(1, 3, true);
-            
-            precisionBelowZeroValidator.Should().Throw<ArgumentException>();
-            scaleBelowZeroValidator.Should().Throw<ArgumentException>();
-            scaleGreaterThanPrecisionValidator.Should().Throw<ArgumentException>();
-            
-            var limitedScaleValidator = new NumberValidator(17, 2, true);
-            var limitedPrecisionValidator = new NumberValidator(3, 2, true);
-            var normalizedNumberValidator = new NumberValidator(4, 2, true);
-            var notOnlyPositiveValidator = new NumberValidator(4, 2, false);
-            
-            limitedScaleValidator.IsValidNumber("0.0").Should().BeTrue();
-            limitedScaleValidator.IsValidNumber("0.000").Should().BeFalse();
-            limitedScaleValidator.IsValidNumber("0").Should().BeTrue();
-            limitedPrecisionValidator.IsValidNumber("00.00").Should().BeFalse();
-            limitedPrecisionValidator.IsValidNumber("+1.23").Should().BeFalse();
-            limitedPrecisionValidator.IsValidNumber("-1.23").Should().BeFalse();
-            limitedPrecisionValidator.IsValidNumber("123").Should().BeTrue();
-            limitedPrecisionValidator.IsValidNumber("123.").Should().BeFalse();
-            normalizedNumberValidator.IsValidNumber("+1.23").Should().BeTrue();
-            normalizedNumberValidator.IsValidNumber("a.sd").Should().BeFalse();
-            normalizedNumberValidator.IsValidNumber("-1.23").Should().BeFalse();
-            normalizedNumberValidator.IsValidNumber("00,00").Should().BeTrue();
-            normalizedNumberValidator.IsValidNumber("1.2.3").Should().BeFalse();
-            normalizedNumberValidator.IsValidNumber("").Should().BeFalse();
-            notOnlyPositiveValidator.IsValidNumber("-1.23").Should().BeTrue();
-            
-        }
+        var numberValidatorConstructor = () => new NumberValidator(precision, scale, onlyPositive);
+        numberValidatorConstructor.Should().Throw<ArgumentException>();
+    }
+    
+    [TestCase(1, 0, true)]
+    [TestCase(2, 1, true)]
+    public void Constructor_NotThrowArgumentException_WhenParametersValid(int precision, int scale, bool onlyPositive)
+    {
+        var numberValidatorConstructor = () => new NumberValidator(precision, scale, onlyPositive);
+        numberValidatorConstructor.Should().NotThrow<ArgumentException>();
+    }
+    
+    [TestCase(17, 2, true, "0.0", true)]
+    [TestCase(17, 2, true, "0.000", false)]
+    [TestCase(17, 2, true, "0", true)]
+    [TestCase(3, 2, true, "00.00", false)]
+    [TestCase(3, 2, true, "+1.23", false)]
+    [TestCase(3, 2, true, "-1.23", false)]
+    [TestCase(3, 2, true, "123", true)]
+    [TestCase(3, 2, true, "123.", false)]
+    [TestCase(4, 2, true, "+1.23", true)]
+    [TestCase(4, 2, true, "a.sd", false)]
+    [TestCase(4, 2, true, "-1.23", false)]
+    [TestCase(4, 2, true, "00,00", true)]
+    [TestCase(4, 2, true, "1.2.3", false)]
+    [TestCase(4, 2, true, ".0", false)]
+    [TestCase(4, 2, true, "", false)]
+    [TestCase(4, 2, true, "    ", false)]
+    [TestCase(4, 2, false, "-1.23", true)]
+    [TestCase(4, 2, false, null, false)]
+    public void IsValidNumber_ReturnExpectedResult(int precision, int scale, bool onlyPositive, string input, bool expected)
+    {
+        new NumberValidator(precision, scale, onlyPositive).IsValidNumber(input).Should().Be(expected);
     }
 }
